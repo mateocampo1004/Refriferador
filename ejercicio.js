27ceedb08da7a6ef830441db49758fb9ae4b8480
jdllanghi <146900595+mateocampo1004@users.noreply.github.com>
2024-02-05 21:37:09 -0500

    Update ejercicio.js

--- ejercicio.js
+++ ejercicio.js
@@ -32,8 +32,15 @@ class Refrigerator{
 
     }
     
-        comida(){
-    
+        comida(producto){
+
+         let posicion = this.meal.indexOf(producto);
+            if(posicion==!-1){
+                this.meal.splice(indice,1);
+                alert(`${producto} se a eliminado de la refrigeradora`);
+            }else{
+                alert('Producto agotado')
+            }
         
     }
 
@@ -42,6 +49,10 @@ class Refrigerator{
 let refri1 = new Refrigerator('Gris',['Cerveza','Pollo','Carne','Leche','Huevos','Yogurt'],'LG','500lb');
 refri1.showInfo();
 
-refri1.compras('Queso');
+refri1.compras('Queso, Jamon');
+
+refri1.showProducts();
+
+refri1.comida('Carne');
 
 refri1.showProducts();
