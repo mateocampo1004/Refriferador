d87bbe6faaaa2520ae6d5e4fa83577f781af1e60
jdllanghi <146900595+mateocampo1004@users.noreply.github.com>
2024-02-05 20:14:19 -0500

    Initial commit

---
+++ ejercicio.js
@@ -0,0 +1,47 @@
+'use strict'
+
+class Refrigerator{
+
+    constructor(color,meal, brand,capacity){
+
+        this.color = color;
+        this.meal = meal;
+        this.brand = brand;
+        this.capacity = capacity;
+    }
+
+    showInfo(){
+
+         alert(`Marca: ${this.brand}`);
+         alert(`Capacidad: ${this.capacity}`);
+         alert(`Calor: ${this.color}`);
+         alert(`Productos: ${this.meal}`);
+      
+
+    }
+
+        showProducts(){
+
+            alert(`Productos: ${this.meal}`);  
+        }
+
+
+    compras(producto){
+
+        this.meal.push(producto); 
+
+    }
+    
+        comida(){
+    
+        
+    }
+
+}
+
+let refri1 = new Refrigerator('Gris',['Cerveza','Pollo','Carne','Leche','Huevos','Yogurt'],'LG','500lb');
+refri1.showInfo();
+
+refri1.compras('Queso');
+
+refri1.showProducts();
