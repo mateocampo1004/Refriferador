3250ac0954089bac9b3bfc2651368691572e23ac
mateocampo1004 <146900595+mateocampo1004@users.noreply.github.com>
2024-02-08 15:28:20 -0500

    Update ejercicio.js

--- ejercicio.js
+++ ejercicio.js
@@ -32,27 +32,17 @@ class Refrigerator{
 
     }
     
-        comida(producto){
-
-         let posicion = this.meal.indexOf(producto);
-            if(posicion==!-1){
-                this.meal.splice(indice,1);
-                alert(`${producto} se a eliminado de la refrigeradora`);
-            }else{
-                alert('Producto agotado')
-            }
+      
         
     }
 
 }
 
 let refri1 = new Refrigerator('Gris',['Cerveza','Pollo','Carne','Leche','Huevos','Yogurt'],'LG','500lb');
+
 refri1.showInfo();
 
-refri1.compras('Queso, Jamon');
+refri1.compras('Queso');
 
 refri1.showProducts();
 
-refri1.comida('Carne');
-
-refri1.showProducts();
